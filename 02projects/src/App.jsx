4f9906d9132c026b50373f64be417e46aea1585a
Harshit Sharma <harshitsharma20006@gmail.com>
2026-01-17 17:30:57 +0530

    new update

---
+++ 02projects/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./components/card";
+import Button from "./components/button";
+
+function App() {
+  return (
+    <>
+    <Button/>
+    {/* <Card name = "Harshit" />
+    <Card name = "vishal"/> */}
+    </>
+    
+  );
+}
+
+export default App;
+
